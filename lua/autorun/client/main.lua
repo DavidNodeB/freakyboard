478e7385f0f1657bc804cce77fd1ff5461b7adc5
DavidNodeB <db1416db@gmail.com>
2025-08-08 15:08:00 -0400

    removed zenui

--- lua/autorun/client/main.lua
+++ lua/autorun/client/main.lua
@@ -1,37 +1,65 @@
 freakboard = freakboard or {}
 
 ----Initialize Zenui----
-local function Initialize()
-    -- print("ZenUI is guaranteed to be initialized here :D")
-end
+-- local function Initialize()
+--     -- print("ZenUI is guaranteed to be initialized here :D")
+-- end
 
-if !_G.ZenUI or !ZenUI.Loaded then
-    hook.Add("ZenUI_Loaded", "FreakyBoard_ZenUI_Loaded", Initialize)
-else
-    Initialize()
-end
+-- if !_G.ZenUI or !ZenUI.Loaded then
+--     hook.Add("ZenUI_Loaded", "FreakyBoard_ZenUI_Loaded", Initialize)
+-- else
+--     Initialize()
+-- end
 ----Initialize Zenui----
+
+surface.CreateFont( "freakylexend", {
+	font = "Lexend", -- Use the font-name which is shown to you by your operating system Font Viewer.
+	extended = false,
+	size = 26,
+	weight = 550,
+	blursize = 0,
+	scanlines = 0,
+	antialias = true,
+	underline = false,
+	italic = false,
+	strikeout = false,
+	symbol = false,
+	rotary = false,
+	shadow = false,
+	additive = false,
+	outline = false,
+} )
+
 function freakboard:show()
     if IsValid(self.frame) then return end -- if frame exist exit func
 
-    self.frame = vgui.Create("ZenUI.Panel")
+    self.frame = vgui.Create("DFrame")
     self.frame:SetSize(ScrW() / 2, ScrH() / 2 + 100)
     self.frame:Center()
+    self.frame:SetTitle("")
+    self.frame:ShowCloseButton(false    )
+    self.frame:SetDraggable(false)
     self.frame:MakePopup()
     self.frame:SetKeyBoardInputEnabled(false)
+
+    self.scrollPanel = vgui.Create("DScrollPanel", self.frame)
+    self.scrollPanel:Dock(FILL)
+    self.scrollPanel:SetSize(500, 500)
+    self.scrollPanel:DockMargin(10, 0, 10, 10)
+
     -- makes board clickable without stoping ply
     -- https://gmodwiki.com/Panel:SetMouseInputEnabled
 
-    -- need ply to be panel
-    -- need panel to have labels
-    self.player = vgui.Create("ZenUI.Dropdown", self.frame)
-    self.player:Dock(TOP)
-    self.player:SetPlaceholder("Name")
-    self.player:AddValue(7)
-
-    for i, v in ipairs(player.GetAll()) do 
-        -- plys
+    for i = 0, 40 do
+        self.button = vgui.Create("DButton", self.scrollPanel)
+        self.button:Dock(TOP)
     end
+
+    self.frame.Paint = function (self, w, h)
+        draw.RoundedBox(4, 0, 0, w, h, Color(25, 25, 25, 255))
+        draw.DrawText( "OracleRP - Scoreboard", "freakylexend", 15, 0, Color( 255, 255, 255, 255 ), TEXT_ALIGN_LEFT )
+    end
+
 end
 
 function freakboard:hide()
