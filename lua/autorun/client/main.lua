a3311ced48bc75f9dad81d6b95024d5e56365639
DavidNodeB <db1416db@gmail.com>
2025-08-08 00:36:33 -0400

    Switched to oop

--- lua/autorun/client/main.lua
+++ lua/autorun/client/main.lua
@@ -12,23 +12,38 @@ else
 end
 ----Initialize Zenui----
 function freakboard:show()
-    if IsValid(self.frame) then return end -- check if frame exist
+    if IsValid(self.frame) then return end -- if frame exist exit func
 
     self.frame = vgui.Create("ZenUI.Panel")
-    self.frame:SetSize(ScrW() / 2, ScrH() / 2)
+    self.frame:SetSize(ScrW() / 2, ScrH() / 2 + 100)
     self.frame:Center()
+    self.frame:MakePopup()
+    self.frame:SetKeyBoardInputEnabled(false)
+    -- makes board clickable without stoping ply
+    -- https://gmodwiki.com/Panel:SetMouseInputEnabled
+
+    -- need ply to be panel
+    -- need panel to have labels
+    self.player = vgui.Create("ZenUI.Dropdown", self.frame)
+    self.player:Dock(TOP)
+    self.player:SetPlaceholder("Name")
+    self.player:AddValue(7)
+
+    for i, v in ipairs(player.GetAll()) do 
+        -- plys
+    end
+end
+
+function freakboard:hide()
+    self.frame:Remove()
 end
 
 
 function freakboard:toggle(toggle)
     if toggle then
         self:show()
-        -- open
     else
-        if IsValid(self.frame) then
-            self.frame:Remove()
-            self.frame = nil
-        end
+        self:hide()
     end
 end
 
