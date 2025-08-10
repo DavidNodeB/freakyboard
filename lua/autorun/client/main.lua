3738d06d0d3e19944b38ad0ca7bd0612ef458a6f
DavidNodeB <db1416db@gmail.com>
2025-08-09 22:00:53 -0400

    added ply count

--- lua/autorun/client/main.lua
+++ lua/autorun/client/main.lua
@@ -47,19 +47,32 @@ function freakboard:show()
     self.scrollPanel:SetSize(500, 500)
     self.scrollPanel:DockMargin(10, 0, 10, 10)
 
+    
+    self.plyCountLabel = vgui.Create("DLabel", self.frame)
+    self.plyCountLabel:SetText(player.GetCount() .. " Players ")    
+    self.plyCountLabel:SetPos(210, 4)
+    self.plyCountLabel:SetFont("freakylexend")
+    self.labelWidth = select(1, self.plyCountLabel:GetTextSize()) -- select(1) just gets the width value 
+    self.plyCountLabel:SetSize(self.labelWidth, 0)
+    self.plyCountLabel:SetAutoStretchVertical(true)
+    
+    print(self.labelWidth)
+
+    self.plyCountLabel.Paint = function (self, w, h)
+        draw.RoundedBox(4, 0, 0, w, h, Color(100, 100, 100, 255))
+    end
+
     -- makes board clickable without stoping ply
     -- https://gmodwiki.com/Panel:SetMouseInputEnabled
 
-    for i = 0, 40 do
-        self.button = vgui.Create("DButton", self.scrollPanel)
-        self.button:Dock(TOP)
-    end
+    -- for _, v in ipairs( player.GetAll() ) do
+    --     self.plyCountLabel:SetText(pla)
+    -- end
 
     self.frame.Paint = function (self, w, h)
         draw.RoundedBox(4, 0, 0, w, h, Color(25, 25, 25, 255))
         draw.DrawText( "OracleRP - Scoreboard", "freakylexend", 15, 0, Color( 255, 255, 255, 255 ), TEXT_ALIGN_LEFT )
     end
-
 end
 
 function freakboard:hide()
